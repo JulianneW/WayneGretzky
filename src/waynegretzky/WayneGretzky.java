71652e32a4859fd42d1aece5a181252c5a7349d9
JuWoj8013 <JuWoj8013@7L0N6Y1.Student.UGDSB.ED>
2018-09-27 10:14:01 -0400

    This is Wayne Gretzky

---
+++ src/waynegretzky/WayneGretzky.java
@@ -0,0 +1,25 @@
+/*
+ * Julianne Wojdak
+ * 18/09/18
+ * This program will display Wayne Gretzky's number
+ */
+
+package waynegretzky;
+
+/**
+ *
+ * @author JuWoj8013
+ */
+public class WayneGretzky {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        double number=99 ;
+        System.out.print ("Wayne Gretzky ");
+        System.out.print("is number " + number );
+        // TODO code application logic here
+    }
+  
+    }
